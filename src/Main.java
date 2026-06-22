69ffd3c426c357dacb9711b38f458880819efeb2
na00111 <angela715@naver.com>
2026-06-22 13:25:59 +0900

    첫 문제

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution(8,9));
+    }
+
+
+
+        public static int solution(int num1, int num2) {
+            int answer = num1 * num2;
+            return answer;
+
+
+        }
+    }
