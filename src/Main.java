9096834de90d9108ff80310c1eb39740b0ba5602
na00111 <angela715@naver.com>
2026-06-25 15:02:45 +0900

    two sum

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Solution sol = new Solution();
+        TwoSum twoSum = new TwoSum();
 
-        int result = sol.solution(7,9);
+        int result = twoSum.solution(7,9);
         System.out.println(result);
 
     }
--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,13 @@
-public class Solution {
-    public int solution( int num1 , int num2) {
-
-        int answer = -1;
-        if(num2 > 0) {
-            answer = num1%num2;
-        }
-
-        return answer;
-            }
-        }
-
-
+//public class Solution {
+//    public int solution( int num1 , int num2) {
+//
+//        int answer = -1;
+//        if(num2 > 0) {
+//            answer = num1%num2;
+//        }
+//
+//        return answer;
+//            }
+//        }
+//
+//
